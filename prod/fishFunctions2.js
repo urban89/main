e2440498f9bd9211a7d4f43759217f71f254b5d9
Istvan Urban <urban89@users.noreply.github.com>
2024-05-26 18:31:24 +0100

    Update fishFunctions2.js

--- prod/fishFunctions2.js
+++ prod/fishFunctions2.js
@@ -124,9 +124,9 @@ document.addEventListener("DOMContentLoaded", function() {
      //home button
      var backToTopButton = document.getElementById("backToTop");
 
-     // Show the button when scrolled down 20px from the top
+     // Show the button when scrolled down 50px from the top
      window.onscroll = function() {
-         if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+         if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
              backToTopButton.style.display = "block";
          } else {
              backToTopButton.style.display = "none";
