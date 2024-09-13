e2a5c22ace7d5b79dea5da8e24728f59c91ada2e
Istvan Urban <urban89@users.noreply.github.com>
2024-09-13 15:09:31 +0100

    Add files via upload

--- prod/modal.js
+++ prod/modal.js
@@ -22,6 +22,8 @@ function showMore(src, id) {
             style.textContent = `
             .more_section {
                text-shadow:0.25px 0.25px 0.25px #f7de1ed8;
+                background: rgba(255, 255, 0, 0.416);
+                border-radius: 5px;
             }
             /* Add more CSS rules here if needed */
           `;
